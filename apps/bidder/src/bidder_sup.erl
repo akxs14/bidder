b8ca91206f4e9a4cbe4d40fa8b8f4f143e65e026
angelos <angelos.kapsimanis@gmail.com>
2014-08-18 01:08:06 +0200

    Add bidder_worker to bidder_sup

--- apps/bidder/src/bidder_sup.erl
+++ apps/bidder/src/bidder_sup.erl
@@ -25,3 +25,50 @@ start_link() ->
 init([]) ->
     {ok, { {one_for_one, 5, 10}, []} }.
 
+%% ---------------------------------------------------------------------------
+%% gen_server Function Exports
+%% ---------------------------------------------------------------------------
+
+-export([                      % The behaviour callbacks
+  init/1,                      % - initializes our process
+  handle_call/3,               % - handles synchronous calls (with response)
+  handle_cast/2,             % - handles asynchronous calls  (no response)
+  handle_cast/3,
+  handle_info/2,               % - handles out of band messages (sent with !)
+  terminate/2,                 % - is called on shut-down
+  code_change/3                % - called to handle code changes
+  ]).
+
+%% ---------------------------------------------------------------------------
+%% API Function Definitions
+%% ---------------------------------------------------------------------------
+
+start_link() ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+stop() ->
+  gen_server:cast(?SERVER, stop).
+
+%% ---------------------------------------------------------------------------
+%% gen_server Function Definitions
+%% ---------------------------------------------------------------------------
+
+init([]) ->
+  RestartStrategy = one_for_one,
+  MaxRestarts = 1000,
+  MaxSecondsBetweenRestarts = 3600,
+
+  SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+  Restart = permanent,
+  Shutdown = 2000,
+  Type = worker,
+
+  Child = {'bidder_worker', {'bidder_worker', start_link, []},
+  Restart, Shutdown, Type, ['bidder_worker']},
+
+  {ok, {SupFlags, [Child]}}.
+
+
+
+
