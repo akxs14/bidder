a5e39972cdf99ef9f18be3d71ee761379404af27
angelos <angelos.kapsimanis@gmail.com>
2014-08-19 00:26:45 +0200

    Add cowboy listeners in bidder_app

--- apps/bidder/src/bidder_app.erl
+++ apps/bidder/src/bidder_app.erl
@@ -10,7 +10,26 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+  Routes  = router:routes(),
+  Dispatch = cowboy_router:compile(Routes),
+  Port = port(),
+  TransOpts = [{port, Port}],
+  ProtoOpts = [{env, [{dispatch, Dispatch}]}],
+  {ok, _} = cowboy:start_http(http, ?C_ACCEPTORS, TransOpts, ProtoOpts),
   bidder_sup:start_link().
 
 stop(_State) ->
   ok.
+
+%% ===================================================================
+%% Internal functions
+%% ===================================================================
+
+port() ->
+    case os:getenv("PORT") of
+        false ->
+            {ok, Port} = application:get_env(http_port),
+            Port;
+        Other ->
+            list_to_integer(Other)
+    end.
