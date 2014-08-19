01bb3cf2dd744f7d84c63bd81ef12d4540c132c2
angelos <angelos.kapsimanis@gmail.com>
2014-08-19 21:05:05 +0200

    Remove route for direct ad serving, it's useless

--- apps/bidder/src/router.erl
+++ apps/bidder/src/router.erl
@@ -1,11 +1,10 @@
 -module(router).
  
--export([routes/0, port/0]).
+-export([routes/0]).
  
 routes() ->
   [
-      {'_', [
-      {"/ad/:userid", ad_request_handler, []},
+    {'_', [
       {"/openrtb2", openrtb2_bid_request_handler, []}
-      ]}
-    ].
+    ]}
+  ].
