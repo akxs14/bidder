0bec5e9bb82cb9722d2f60d37308f6ac2a1a6fea
angelos <angelos.kapsimanis@gmail.com>
2014-11-05 23:56:09 +0100

    Added Bid Object assembly

--- apps/bidder/src/bidswitch_bid_request_handler.erl
+++ apps/bidder/src/bidswitch_bid_request_handler.erl
@@ -35,12 +35,16 @@ terminate(_Reason, _Req, _State) ->
 handle_bid_request(Req, State) ->
   {ok, Body, _Req1} = cowboy_req:body(Req),
   BidRequest = openrtb2_bid_request_parser:parse(Body),
-  {ToBid, BidResponse, Bid} = decision_engine_worker:decide(BidRequest),
+  {ToBid, SelectedCampaign, Bid} = decision_engine_worker:decide(BidRequest),
   case ToBid of
     no_bid ->
       {ok, Id} = maps:get(id, BidRequest),
       HTTPResponse = bidswitch_bid_response_builder:reply_no_bid(Id, Req);
     bid ->
-      HTTPResponse = bidswitch_bid_response_builder:reply_bid(Req, BidResponse, BidRequest, Bid)
+      HTTPResponse = bidswitch_bid_response_builder:reply_bid(
+        Req,
+        BidRequest,
+        SelectedCampaign,
+        Bid)
   end,
   {halt, HTTPResponse, State}.
--- apps/bidder/src/bidswitch_bid_response_builder.erl
+++ apps/bidder/src/bidswitch_bid_response_builder.erl
@@ -7,7 +7,7 @@
 %% ============================================================================
 
 -define(SEATID, "123456").
--define(BIDID, "123456").
+
 
 %% ============================================================================
 %% API functions
@@ -20,10 +20,10 @@ reply_no_bid(Id, Req) ->
     Req),
   Req2.
 
-reply_bid(Req, BidResponse, BidRequest, Bid) ->
+reply_bid(Req, BidRequest, SelectedCampaign, Bid) ->
   {ok, Req2} = cowboy_req:reply(200, 
     [{<<"content-type">>, <<"application/json">>}], 
-    maps:to_json(extract_bid_req_data(BidRequest)),
+    maps:to_json(prepare_bid_response(BidRequest, SelectedCampaign, Bid)),
      % jiffy:encode(<<"{\"rest\": \"Hello World!!!!\"}">>), 
     Req ),
   Req2.
@@ -39,24 +39,52 @@ prepare_no_bid_response(Id) ->
       seatbid => []
   }.
 
-prepare_bid_response(BidRequest) ->
+prepare_bid_response(BidRequest, SelectedCampaign, Bid) ->
+  ProductId = "XXXX",
   { Id, Currency } = extract_bid_req_data(BidRequest),
+  { ImpId } = extract_bid_data(BidRequest),
+  { UserId } = extract_user_data(BidRequest),
   #{
       cur => Currency,
       id => Id,
-      seatbid => [{
-        bid => [{
-          id => "1"
+      seatbid => [#{
+        seat => ?SEATID,
+        bid => [#{
+          id => generate_bid_id(),
+          impid => ImpId,
+          price => 10,
+          nurl => assemble_ad_server_url(SelectedCampaign, UserId, ProductId),
+          adid => SelectedCampaign,
+          adomain => "www.attalon.com",
+          cid => SelectedCampaign  
         }]
-        seat => ?SEATID
       }]
   }.
+
+assemble_ad_server_url(SelectedCampaign, UserId, ProductId) ->
+  lists:flatten(io_lib:format(
+    "http://adsrv~p.attalon.com/~p/~p", [SelectedCampaign, UserId, ProductId])).
 
 extract_bid_req_data(BidRequest) ->
   #{ id := Id, cur := Currency } = BidRequest,
   { Id, Currency }.
 
 
-extract_bd_data(BidRequest) ->
-  #{  } = BidRequest.
+extract_bid_data(BidRequest) ->
+  #{ impressions := Impressions } = BidRequest,
+  [Impression | _] = Impressions,
+  #{ id := ImpId } = Impression,
+  { ImpId }.
+
+
+generate_bid_id() ->
+  {MegaSecs, Secs, MicroSecs} = erlang:now(),
+  lists:flatten(io_lib:format("~p~p~p", [MegaSecs, Secs, MicroSecs])).
+
+
+extract_user_data(BidRequest) ->
+  #{ user := User } = BidRequest,
+  #{ id := UserId } = User,
+  { UserId }.
+
 
