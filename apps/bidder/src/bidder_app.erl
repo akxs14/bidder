cc1ee6681f0376ffc52847bed142319ce8c1f34d
angelos <angelos.kapsimanis@gmail.com>
2014-12-01 22:41:06 +0100

    KISS, KISS, KISS

--- apps/bidder/src/bidder_app.erl
+++ apps/bidder/src/bidder_app.erl
@@ -1,22 +1,23 @@
 -module(bidder_app).
-
 -behaviour(application).
 
 %% Application callbacks
 -export([start/2, stop/1]).
 
--define(C_ACCEPTORS,  100).
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
-start(_StartType, _StartArgs) ->
-  Routes  = router:routes(),
-  Dispatch = cowboy_router:compile(Routes),
-  Port = port(),
-  TransOpts = [{port, Port}],
-  ProtoOpts = [{env, [{dispatch, Dispatch}]}],
-  {ok, _} = cowboy:start_http(http, ?C_ACCEPTORS, TransOpts, ProtoOpts),
+start(_Type, _Args) ->
+  Dispatch = cowboy_router:compile([
+          {'_', [
+                  {"/openrtb2",  openrtb2_bid_request_handler,  []},
+                  {"/bidswitch", bidswitch_bid_request_handler, []}
+          ]}
+  ]),
+  {ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
+          {env, [{dispatch, Dispatch}] }
+  ]),
   bidder_sup:start_link().
 
 stop(_State) ->
@@ -25,12 +26,3 @@ stop(_State) ->
 %% ===================================================================
 %% Internal functions
 %% ===================================================================
-
-port() ->
-    case os:getenv("PORT") of
-        false ->
-            {ok, Port} = application:get_env(http_port),
-            Port;
-        Other ->
-            list_to_integer(Other)
-    end.
